3c4ce5c387d4a9195d06db21163a20aad6886072
joao <joaopedropsales@hotmail.com>
2024-09-11 23:37:32 -0300

    initial commit

---
+++ script.js
@@ -0,0 +1,15 @@
+document.getElementById('btnHome').addEventListener('click', function () {
+    document.getElementById('home').scrollIntoView({ behavior: 'smooth' });
+});
+
+document.getElementById('btnAbout').addEventListener('click', function () {
+    document.getElementById('about').scrollIntoView({ behavior: 'smooth' });
+});
+
+document.getElementById('btnProjects').addEventListener('click', function () {
+    document.getElementById('projects').scrollIntoView({ behavior: 'smooth' });
+});
+
+document.getElementById('btnContact').addEventListener('click', function () {
+    document.getElementById('contact').scrollIntoView({ behavior: 'smooth' });
+});
