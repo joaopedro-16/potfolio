be0d59fa8729034226fa105becaa0092e33ed082
joao <joaopedropsales@hotmail.com>
2024-09-13 02:06:02 -0300

    general updates

--- script.js
+++ script.js
@@ -13,3 +13,25 @@ document.getElementById('btnProjects').addEventListener('click', function () {
 document.getElementById('btnContact').addEventListener('click', function () {
     document.getElementById('contact').scrollIntoView({ behavior: 'smooth' });
 });
+
+let darkmode = localStorage.getItem('darkmode')
+const themeSwitch = document.getElementById('theme-switch')
+
+const enableDarkmode = () => {
+    document.body.classList.add('darkmode')
+    localStorage.setItem('darkmode', 'active')
+    document.getElementById('profilePhoto').src = 'assets/foto-perfil-dark.png'
+}
+
+const disableDarkmode = () => {
+    document.body.classList.remove('darkmode')
+    localStorage.setItem('darkmode', null)
+    document.getElementById('profilePhoto').src = 'assets/foto-perfil-light.png'
+}
+
+if(darkmode === "active") enableDarkmode()
+
+themeSwitch.addEventListener("click", () => {
+    darkmode = localStorage.getItem('darkmode')
+    darkmode !== "active" ? enableDarkmode() : disableDarkmode()
+})
